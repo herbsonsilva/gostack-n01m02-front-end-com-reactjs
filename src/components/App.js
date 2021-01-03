314d82fb6b0a2003d014ba2543f9ca46f6b1c2f6
Herbson Silva <herbsonsilvajp@gmail.com>
2021-01-02 22:09:13 -0300

    Added components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from './Header';
+import Menu from './Menu';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Menu />
+    </>
+  );
+}
---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Menu() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
