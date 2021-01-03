67c209313829c39d603a2efbffc069236acc5593
Herbson Silva <herbsonsilvajp@gmail.com>
2021-01-02 22:58:42 -0300

    Added state

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 import Menu from './Menu';
 
 export default function App() {
+  const [techs, setTechs] = useState(['Node.js', 'ReactJS']);
+
+  function handleAddTech() {
+    setTechs([...techs, `New Trader ${ Date.now() }`]);
+  }
+
   return (
     <>
-      <Header title="Meu Site">
-        <ul>
-          <li>Link</li>
-        </ul>
-      </Header>
+      <Header title="Meu Site" />
       <Menu />
+      <ul>
+        { techs.map(tech => (
+          <li key={tech}>{ tech }</li>
+        )) }
+      </ul>
+      <button type="button" onClick={handleAddTech}>Adicionar tecnologia</button>
     </>
   );
 }
